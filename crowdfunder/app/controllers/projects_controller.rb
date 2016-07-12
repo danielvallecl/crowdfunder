34b57839fa5786f3d5b3cd34b88f74cb7101ae8d
Daniel Valle <daniel.vallecl@gmail.com>
2016-07-12 19:02:27 -0400

    Finished Show Users

--- crowdfunder/app/controllers/projects_controller.rb
+++ crowdfunder/app/controllers/projects_controller.rb
@@ -6,11 +6,10 @@ class ProjectsController < ApplicationController
 
   def new
     @project = Project.new
-    render :new                             #always a symbol
   end
 
   def create
-    @project = Project.new
+    @project = Project.new(project_params)
 
     if @project.save
       redirect_to projects_path
@@ -23,6 +22,10 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
-
+  private
+  def project_params
+    puts params
+    params.require(:project).permit(:title, :description, :goal, :deadline)
+  end
 
 end
--- crowdfunder/config/routes.rb
+++ crowdfunder/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :projects          #Provides all the CRUD methods##
 
+  resources :users            #Resources includes all RESTful methods
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
