45b7dd29b27ea46c452672b9a65ab0873c4877ff
andklar <andrewklar@hotmail.com>
2016-07-13 18:22:42 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-	root :to => 'users#index'
+    root 'projects#index'
 	resources :sessions
 	resources :users do
 		resources :pledges, only: [:show]
 	end
-	resources :projects
-	resources :rewards
-
+    resources :projects do
+      resources :rewards
+    end
 	get 'login' => 'sessions#new', :as => :login
 	post 'logout' => 'sessions#destroy', :as => :logout
 
