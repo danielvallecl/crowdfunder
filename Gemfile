ab392f4da742205e7944c248c9ae4e146b8c5b37
Rigil-Kent <tsubashi.gurung@gmail.com>
2016-07-14 15:04:30 -0400

    added bootstap gem

--- Gemfile
+++ Gemfile
@@ -18,6 +18,10 @@ gem 'coffee-rails', '~> 4.2'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+
+# bootstrap 
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,2 +1,36 @@
 class RewardsController < ApplicationController
+
+  def new
+    @reward = Reward.new
+  end
+
+  def show
+    @reward = Reward.all
+  end
+
+  def create
+    @reward = Reward.new(reward_params)
+
+    respond_to do |format|
+      if @.save
+        format.html { redirect_to @user, notice: 'Reward was successfully created.' }
+        format.json { render :show, status: :created }
+      else
+        format.html { render :new }
+        format.json { render json: @reward.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_reward
+      @reward = Reward.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:reward).permit(:amount, :description)
+    end
 end
