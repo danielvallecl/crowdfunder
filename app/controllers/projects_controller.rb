93d956e4be7fbcade73762163aee0994f68867db
andklar <andrewklar@hotmail.com>
2016-07-15 14:41:41 -0400

    refactored show and index pages.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,29 +21,43 @@ class ProjectsController < ApplicationController
 
   def update
     @project = Project.find(params[:id])
-    if @project.update_attributes(project_params)
-      redirect_to project_path(@project)
-    else
-      render :edit
+    respond_to do |format|
+      if @project.update_attributes(project_params)
+        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+        format.json { render :show, status: :updated, location: @project }
+        redirect_to project_path(@project)
+      else
+        format.html { render :edit }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
     end
   end
+
 
   def create
     @project = Project.new(project_params)
 
     @project.owner_id = current_user.id
 
-    if @project.save
-      redirect_to project_path(@project)
-    else
-      render :new
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to @project, notice: 'Project was successfully created.' }
+        format.json { render :show, status: :created, location: @project }
+      else
+        format.html { render :new }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+
+      end
     end
   end
 
   def destroy
     @project = Project.find(params[:id])
     @project.destroy
-    redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: 'Project was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end  
 
   private
